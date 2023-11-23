023ab74a4d3206496402ce8281cc4c0593b1114d
ajsha <ajsha@LAPTOP-1F3RIS2D>
2023-11-23 18:04:09 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Project_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LstCommands = new System.Windows.Forms.ListBox();
+            this.TmrSpecking = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LstCommands
+            // 
+            this.LstCommands.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LstCommands.Dock = System.Windows.Forms.DockStyle.Right;
+            this.LstCommands.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.LstCommands.FormattingEnabled = true;
+            this.LstCommands.ItemHeight = 16;
+            this.LstCommands.Location = new System.Drawing.Point(514, 0);
+            this.LstCommands.Name = "LstCommands";
+            this.LstCommands.Size = new System.Drawing.Size(290, 567);
+            this.LstCommands.TabIndex = 0;
+            this.LstCommands.Visible = false;
+            // 
+            // TmrSpecking
+            // 
+            this.TmrSpecking.Enabled = true;
+            this.TmrSpecking.Interval = 1000;
+            this.TmrSpecking.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(487, 342);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(804, 567);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LstCommands);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstCommands;
+        private System.Windows.Forms.Timer TmrSpecking;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace Project_1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(); 
+        SpeechSynthesizer sarah = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+        DateTime TimeNow = DateTime.Now;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefalutCommands.txt")))));
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(recognizer_SpeechRecognized);
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefalutCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+        }
+
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "")
+            {
+                sarah.SpeakAsyncCancelAll();
+            }
+            if (speech == "Hello")
+            {
+                sarah.SpeakAsync("Hello, I am here");
+            }
+            if (speech == "Hii")
+            {
+                sarah.SpeakAsync("Hello, Master ");
+            }
+            if (speech == "How are you")
+            {
+                sarah.SpeakAsync("I am working normally");
+            }
+            if (speech == "What time is it")
+            {
+                sarah.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Stop Talking")
+            {
+                sarah.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1);
+                if (ranNum == 1)
+                {
+                    sarah.SpeakAsync("Yes Sir");
+                }
+                
+            }
+            if (speech == "Stop Listening")
+            {
+                sarah.SpeakAsync("if you need me just  ask");
+                recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+
+            if (speech == "Show Commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefalutCommands.txt"));
+                LstCommands.Items.Clear();
+                LstCommands.SelectionMode = SelectionMode.None;
+                LstCommands.Visible = true;
+                foreach (string command in commands)
+                {
+                    LstCommands.Items.Add(command);
+                }
+            }
+
+            if (speech == "Hide Commands")
+            {
+                LstCommands.Visible =false;
+            }
+        }
+        private void recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;  
+
+            if(speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                sarah.SpeakAsync("Yes, I am here ");
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(RecTimeOut== 10)
+            {
+                recognizer.RecognizeAsyncCancel();
+            }
+            else if(RecTimeOut== 11)
+            {
+                TmrSpecking.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut= 0;
+            }
+        }
+    }
+}
